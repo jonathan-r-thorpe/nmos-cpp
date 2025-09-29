99b202a12e093f7b7c21bb19b9a60aa606d05fad
jonathan-r-thorpe <jonathan.thorpe@sony.com>
2025-09-29 17:25:15 +0100

    Pass in script directory

--- Sandbox/run_registry_stress_test.sh
+++ Sandbox/run_registry_stress_test.sh
@@ -8,6 +8,8 @@ results_dir=$1
 shift
 host_ip=$1
 shift
+script_dir=$1
+shift
 
 common_params=",\
   \"logging_level\":-40,\
@@ -22,7 +24,7 @@ registry_params=",\
 "${registry_command}" "{\"pri\":0,\"http_port\":8088 ${common_params} ${registry_params}}" > ${results_dir}/registryoutput_stresstest 2>&1 &
 REGISTRY_PID=$!
 
-"${run_python} registry_stress_test.py --host ${host_ip} --port 8088" >> ${results_dir}/stresstest 2>&1
+"${run_python} ${script_dir}/registry_stress_test.py --host ${host_ip} --port 8088" >> ${results_dir}/stresstest 2>&1
 
 # Stop Node and Registry
 kill $REGISTRY_PID || echo "registry not running"
