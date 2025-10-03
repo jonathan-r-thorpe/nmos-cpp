8c0bd67424d9d8b570815d903eb2e57862ce4a9c
jonathan-r-thorpe <jonathan.thorpe@sony.com>
2025-10-03 16:36:43 +0100

    Debug python problems......

--- Sandbox/run_registry_stress_test.sh
+++ Sandbox/run_registry_stress_test.sh
@@ -24,7 +24,6 @@ registry_params=",\
 "${registry_command}" "{\"pri\":0,\"http_port\":8088 ${common_params} ${registry_params}}" > ${results_dir}/registryoutput_stresstest 2>&1 &
 REGISTRY_PID=$!
 
-cd ${script_dir}
 #"${run_python} ${script_dir}/registry_stress_test.py --host ${host_ip} --port 8088" >> ${results_dir}/stresstest 2>&1
 #"${run_python} --version" >> ${results_dir}/stresstest 2>&1
 result=$(${run_python} ${script_dir}/registry_stress_test.py --host ${host_ip} --port 8088 >> ${results_dir}/stresstest 2>&1; echo $?)
